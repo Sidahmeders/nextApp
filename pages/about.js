a26d2d901bde5b115baf118d575dfceb150e9bbb
sidahmed <ahorob57@gmail.com>
2020-10-14 21:34:32 +0100

    inital commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+
+let About = () => (
+    <div>
+        <h1>about the nextJS world?</h1>
+        <div>
+            <ul>
+                <li>cool world</li>
+                <li>the best of e now</li>
+            </ul>
+        </div>
+    </div>
+);
+
+export default About;
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+let Contact = () => {
+    return (
+        <div>
+            <h1>phone: +83729428781934</h1>
+            <h1>Email: gonzaloSimon@email.com</h1>
+            <h1>LinkedIn: sidokalold</h1>
+        </div>
+    );
+}
+
+export default Contact;
